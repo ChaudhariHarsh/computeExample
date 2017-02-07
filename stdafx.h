d862d9689a999c7889ab404f289360e468910bd3
Nilay <nilay.engineer@iiitb.ac.in>
2017-02-07 14:23:31 +0530

    add camera class

---
+++ stdafx.h
@@ -0,0 +1,28 @@
+//GL shit
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include<GL/glew.h>
+#include<GL/glu.h>
+#include<GL/gl.h>
+//std shit
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<fstream>
+#include<memory>
+//SDL shit
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_opengl.h>
+
+//GLM shit
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+
+//const int FPS = 30;
+//#define GLSL(src) "#version 150 core\n" #src

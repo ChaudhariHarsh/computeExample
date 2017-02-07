bac0598436550a6b9a89379952cf3dd3a90265c5
Nilay <nilay.engineer@iiitb.ac.in>
2017-02-07 14:25:42 +0530

    Set up shaders in main file

--- computeExample.cpp
+++ computeExample.cpp
@@ -1,11 +1,22 @@
-#include<SDL2/SDL.h>
-#include<GL/glew.h>
-#include<GL/glu.h>
-#include<iostream>
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <GL/glu.h>
+#include <glm/glm.hpp>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <memory>
+
+#include "camera.h"
+#include "ShaderProgram.hpp"
+#include "utils.h"
 
 using namespace std;
 
 GLuint VAO, VBO;
+GLuint computeShader,computeProgram;
 SDL_Event e;
 SDL_Window* window = nullptr;
 bool quit = false;
@@ -14,7 +25,7 @@ int main()
 {
 #pragma region SDL_INIT
 	
-	Uint32 start = NULL;
+	//int start = NULL;
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
@@ -36,6 +47,57 @@ int main()
 
 #pragma endregion SDL_INIT
 
+#pragma region CAMERA_CODE
+
+	Camera cam;
+	cam.SetPosition(glm::vec3(0, 0, 35));
+	int m = 4;
+#pragma endregion CAMERA_CODE
+
+#pragma region SHADERS
+	unique_ptr<ShaderProgram> viewShader(new ShaderProgram());
+	viewShader->initFromFiles("render.vert","render.frag");
+	viewShader->addAttribute("pos");
+	viewShader->addUniform("view");
+	viewShader->addUniform("projection");
+
+	//now setup compute shader
+	ifstream file("compute.cs");
+	if (!file.good())
+	{
+		throw std::runtime_error("Failed to open file: compute.cs");
+	}
+	stringstream stream;
+	const char *source;
+	int length;
+
+	stream << file.rdbuf();
+	file.close();
+	string cs_shader_src = stream.str();
+	computeShader = glCreateShader(GL_COMPUTE_SHADER);
+	source = cs_shader_src.c_str();
+	length = cs_shader_src.length();
+	glShaderSource(computeShader, 1, &source, &length);
+	glCompileShader(computeShader);
+	//check_program_compile_status(computeShader);
+	computeProgram = glCreateProgram();
+	glAttachShader(computeProgram, computeShader);
+	glLinkProgram(computeProgram);
+	GLint status;
+    glGetProgramiv(computeProgram, GL_LINK_STATUS, &status);
+    if(status == GL_FALSE) {
+        GLint length;
+        glGetProgramiv(computeProgram, GL_INFO_LOG_LENGTH, &length);
+        std::vector<char> log(length);
+        glGetProgramInfoLog(computeProgram, length, &length, &log[0]);
+        std::cerr << &log[0];
+        return false;
+    }
+    //check_program_link_status(acceleration_program);
+	//By this point out compute shader must be created.
+
+#pragma endregion SHADERS
+
 	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);	//clear screen
 	glClear(GL_COLOR_BUFFER_BIT);
 	
@@ -43,6 +105,7 @@ int main()
 	{
 		while (SDL_PollEvent(&e) != 0)
 		{
+			glClear(GL_COLOR_BUFFER_BIT);
 			switch (e.type)
 			{
 			case SDL_QUIT:	//if X windowkey is pressed then quit
@@ -67,8 +130,12 @@ int main()
 				
 			}
 		}
-
+		//glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window);
 	}
-	SDL_GL_SwapWindow(window);
+	//Exit
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+    SDL_Quit();
 
 }
